3bb3b23caca68c825f2dc6fbfff0bf76806a7743
dwiPriyatmoko <dwipriyatmoko27@gmail.com>
2026-02-14 21:57:23 +0700

    feat: update files to ignore

--- .vercel/output/static/_nuxt/6kzkZDIX.js
+++
@@ -1,1 +0,0 @@
-import{p as s}from"./on5csu9o.js";const i=s("/assets/images/sliders/slider_11.jpg");export{i as _};
--- .vercel/output/static/_nuxt/CQEY4h87.js
+++
@@ -1,1 +0,0 @@
-import"./on5csu9o.js";const e=""+new URL("nebo_logo.DxzHPB3y.jpeg",import.meta.url).href;export{e as _};
